f13815c96b7e3753d2de39efb416368560786436
Honghua Zhu <zhh200910@gmail.com>
2020-11-19 15:36:46 +0800

    重构orm-demo的代码

--- orm-demo/src/main/java/org/lealone/examples/orm/OrmDemo.java
+++ orm-demo/src/main/java/org/lealone/examples/orm/OrmDemo.java
@@ -17,73 +17,113 @@
  */
 package org.lealone.examples.orm;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import org.lealone.db.Constants;
-import org.lealone.db.SysProperties;
-import org.lealone.transaction.TransactionEngine;
-import org.lealone.transaction.TransactionEngineManager;
-import org.lealone.transaction.aote.log.LogSyncService;
+import org.lealone.examples.orm.generated.Customer;
+import org.lealone.examples.orm.generated.Order;
 
 public class OrmDemo {
 
-    // 通过JDBC访问的数据库的URL
-    static String jdbcUrl = "jdbc:lealone:embed:test";
-
     public static void main(String[] args) throws Exception {
-        initTransactionEngine();
-
         createTable();
-
-        CrudTest.run();
+        testCrud();
+        testJoin();
+        testTransaction();
     }
 
-    public static void createTable() throws Exception {
+    // ------------ 以下代码中出现的的 where()都是可选的 ------------
+
+    // 单表crud操作
+    static void testCrud() {
+        // insert
+        new Customer().id.set(1001).name.set("rob").phone.set(12345678).insert();
+
+        Customer dao = Customer.dao;
+
+        // find one
+        Customer c = dao.where().id.eq(1001).findOne();
+        assertEquals("rob", c.name.get());
+
+        // find list
+        List<Customer> list = dao.id.eq(1001).findList();
+        assertEquals(1, list.size());
+
+        // count
+        int count = dao.findCount();
+        assertEquals(1, count);
+
+        // 单记录 update
+        c.notes.set("test").update();
+        c = dao.where().id.eq(1001).findOne();
+        assertEquals("test", c.notes.get());
+
+        // 批量 update
+        dao.notes.set("batch update").where().name.startsWith("rob").update();
+        c = dao.id.eq(1001).findOne();
+        assertEquals("batch update", c.notes.get());
+
+        // 分页查询
+        list = dao.offset(1).limit(1).findList();
+        assertEquals(1, list.size());
+
+        // delete
+        dao.where().id.eq(1001).delete();
+        count = dao.findCount();
+        assertEquals(0, count);
+    }
+
+    // 两表关联查询
+    static void testJoin() {
+        // 批量增加有关联的记录
+        Order o1 = new Order().orderId.set(2001).orderDate.set("2018-01-01");
+        Order o2 = new Order().orderId.set(2002).orderDate.set("2018-01-01");
+        Customer customer = new Customer().id.set(1002).name.set("customer1");
+        customer.addOrder(o1, o2).insert();
+        // 调用addOrder后，Order的customerId字段会自动对应Customer的id字段
+        assertEquals(o1.customerId.get(), customer.id.get());
+
+        // 关联查询
+        Customer c = Customer.dao;
+        Order o = Order.dao;
+        customer = c.join(o).on().id.eq(o.customerId).where().id.eq(1002).findOne();
+
+        // 一个customer对应两个Order
+        List<Order> orderList = customer.getOrderList();
+        assertEquals(2, orderList.size());
+        assertTrue(customer == orderList.get(0).getCustomer());
+    }
+
+    // 测试事务
+    static void testTransaction() {
+        try {
+            Customer.dao.beginTransaction(); // 开始一个新事务
+
+            new Customer().id.set(1003).name.set("rob3").insert();
+            new Customer().id.set(1004).name.set("rob4").insert();
+
+            Customer.dao.commitTransaction(); // 提交事务
+        } catch (Exception e) {
+            Customer.dao.rollbackTransaction(); // 回滚事务
+            e.printStackTrace();
+        }
+
+        int count = Customer.dao.where().id.eq(1003).or().id.eq(1004).findCount();
+        assertEquals(2, count);
+    }
+
+    // 执行建表脚本，同时自动生成对应的模型类的代码
+    static void createTable() throws Exception {
+        String jdbcUrl = "jdbc:lealone:embed:test";
         System.setProperty("lealone.jdbc.url", jdbcUrl);
 
-        execute("drop table if exists `order`");
-        execute("drop table if exists customer");
-
-        System.out.println("create table: customer");
-
-        // 创建表: customer，会生成一个名为Customer的模型类
-        String sql = "create table if not exists customer(id long primary key, name char(10), notes varchar, phone int)" //
-                + "   package 'org.lealone.examples.orm.generated'" // Customer类所在的包名
-                + "   generate code './src/main/java'"; // Customer类的源文件所在的根目录
-        execute(sql);
-
-        System.out.println("create table: order");
-
-        // order是关键字，所以要用特殊方式表式
-        // 创建表: user，会生成一个名为User的模型类
-        sql = "create table if not exists `order`(customer_id long, order_id int primary key, order_date date, total double,"
-                + "   FOREIGN KEY(customer_id) REFERENCES customer(id))" //
-                + "   package 'org.lealone.examples.orm.generated'" // User类所在的包名
-                + "   generate code './src/main/java'"; // User类的源文件所在的根目录
-        execute(sql);
-    }
-
-    public static void execute(String sql) throws Exception {
-        Connection conn = DriverManager.getConnection(jdbcUrl);
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate(sql);
-        stmt.close();
-        conn.close();
-    }
-
-    public static void initTransactionEngine() {
-        SysProperties.setBaseDir("./target/db_base_dir");
-        TransactionEngine te = TransactionEngineManager.getInstance()
-                .getEngine(Constants.DEFAULT_TRANSACTION_ENGINE_NAME);
-
-        Map<String, String> config = new HashMap<>();
-        config.put("base_dir", "./target/db_base_dir");
-        config.put("redo_log_dir", "redo_log");
-        config.put("log_sync_type", LogSyncService.LOG_SYNC_TYPE_PERIODIC);
-        te.init(config);
+        try (Connection conn = DriverManager.getConnection(jdbcUrl); Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("RUNSCRIPT FROM './src/main/resources/tables.sql'");
+        }
     }
 }
---
+++ orm-demo/src/main/resources/tables.sql
@@ -0,0 +1,24 @@
+-- 删除表
+drop table if exists `order`;
+drop table if exists customer;
+
+set @packageName 'org.lealone.examples.orm.generated'; -- 生成的模型类所在的包名
+set @srcPath './src/main/java'; -- 生成的模型类对应的源文件所在的根目录
+
+-- 创建customer表，会生成一个名为Customer的模型类
+create table if not exists customer (
+  id long primary key,
+  name char(10),
+  notes varchar,
+  phone int
+) package @packageName generate code @srcPath;
+
+-- 创建order表，会生成一个名为Order的模型类
+-- order是关键字，所以要用特殊方式表式
+create table if not exists `order` (
+  customer_id long,
+  order_id int primary key,
+  order_date date,
+  total double,
+  FOREIGN KEY(customer_id) REFERENCES customer(id)
+) package @packageName generate code @srcPath;
