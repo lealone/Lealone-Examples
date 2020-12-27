efe9e0b9b704337f6cc529e54cd7b762847f74c6
Honghua Zhu <zhh200910@gmail.com>
2020-12-27 20:24:07 +0800

    login直接post json数据到后端

--- petstore/petstore-web/web/user/js/login.js
+++ petstore/petstore-web/web/user/js/login.js
@@ -1,18 +1,18 @@
-﻿	const userLogin = {
-	  data() {
-		return {
-		  name: "",
-		  password: ""
+﻿ const userLogin = { 
+    data() {
+        return {
+            user: {user_id: "", password: ""}
+        }
+    },
+    methods: {
+        login() {
+            axios.post(Public.UserService + '/login', this.user)
+			.then(function (response) {
+				location.href = "/";
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 		}
-	  },
-	  methods: {
-		login() {
-		  axios
-		  .post(Public.UserService + '/login?password=' + this.password + '&user_id=' + this.name, {
-		  name: this.name,
-		  password: this.password
-		}) //this.$data
-		  .then(response => (this.categories = response.data))
-		}
-	  }
-	}
+    }
+}
