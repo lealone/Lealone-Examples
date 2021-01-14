5cbfbdb63f86cdb39ab5d46951c1adfdc1fb60b6
Honghua Zhu <zhh200910@gmail.com>
2021-01-14 23:18:08 +0800

    整理代码

--- lealone-opscenter/opscenter-web/web/ops/js/login.js
+++ lealone-opscenter/opscenter-web/web/ops/js/login.js
@@ -1,10 +1,16 @@
 ﻿const opsLogin = { 
     data() {
         return {
+            name: "",
+            driver: "",
 			url: "jdbc:lealone:tcp://localhost:9210/lealone",
 		    user: "root",
 			password: "",
-            languages: []
+			language: "en",
+            languages: [],
+            setting: "",
+            settings: [],
+            errorMessage: "",
         }
     },
     methods: {
@@ -13,20 +19,28 @@
             axios.post(OpsCenter.OpsService + '/login', { url: this.url, user: this.user, password: this.password })
             .then(function (response) {
                 console.log(response.data);
-				// that.lealone.route("ops", "header");
-				that.lealone.route("ops", "tables");
-                // location.href = "/";
+                location.href = "/ops/ops.html";
             })
             .catch(function (error) {
                 console.log(error);
                 that.errorMessage = "用户名或密码不正确,请重新输入";
             });
+        },
+        changeLanguage() {
+            var that = this;
+            axios.get(OpsCenter.OpsService + '/get_language_combo')
+            .then(response => {
+                that.languages = response.data;
+            });
+        },
+        testConnection() {
+        },
+        settingSave() {
+        },
+        settingRemove() {
         }
     },
     mounted() {
-        axios.get(OpsCenter.OpsService + '/get_language_combo')
-        .then(response => {
-			this.languages = response.data;
-		});
+        this.changeLanguage();
     }
 }
