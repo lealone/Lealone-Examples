7230921059a194ab867194d5c709fc9af9b89925
Honghua Zhu <zhh200910@gmail.com>
2022-04-26 13:37:19 +0800

    update build.sh

--- petstore/build.sh
+++ petstore/build.sh
@@ -2,4 +2,3 @@
 mvn package -Dmaven.test.skip=true
 java -cp petstore-dist/target/lealone-petstore-1.0.0.jar org.lealone.server.template.TemplateCompiler -webRoot petstore-web/web -targetDir petstore-dist/target
 mvn assembly:assembly -Dmaven.test.skip=true
-
