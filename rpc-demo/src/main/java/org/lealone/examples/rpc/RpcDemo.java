b5608c3d696e1961233fac2313f0a70c76e6d4e9
Honghua Zhu <zhh200910@gmail.com>
2022-12-22 17:23:42 +0800

    update rpc-demo

--- rpc-demo/src/main/java/org/lealone/examples/rpc/RpcDemo.java
+++ rpc-demo/src/main/java/org/lealone/examples/rpc/RpcDemo.java
@@ -18,29 +18,19 @@
 package org.lealone.examples.rpc;
 
 import org.lealone.main.Lealone;
-import org.lealone.plugins.service.http.HttpServer;
 
+// 请在浏览器中打开下面的 URL 进行测试:
+// http://localhost:9000/service/hello_service/say_hello?name=zhh
+// http://localhost:9000/hello.html
 public class RpcDemo {
 
-    // 通过 JDBC 访问的数据库的 URL
-    static String jdbcUrl = "jdbc:lealone:embed:test";
-
     public static void main(String[] args) {
-        // 静态资源文件的根目录，如果有多个可以用逗号分隔
-        String webRoot = args.length == 1 ? args[0] : "./web";
-
-        // 启动 HttpServer，请在浏览器中打开下面的 URL 进行测试:
-        // http://localhost:8080/hello.html
-        HttpServer server = HttpServer.create();
-        server.setJdbcUrl(jdbcUrl);
-        server.setWebRoot(webRoot);
-        server.start();
-
-        createService();
+        Lealone.main(args, () -> runScript());
     }
 
-    // 执行 services.sql 脚本，创建服务
-    public static void createService() {
-        Lealone.runScript(jdbcUrl, "./src/main/resources/services.sql");
+    public static void runScript() {
+        String url = "jdbc:lealone:tcp://localhost:9210/lealone?user=root";
+        // 执行 services.sql 脚本，创建服务
+        Lealone.runScript(url, "./sql/services.sql");
     }
 }
